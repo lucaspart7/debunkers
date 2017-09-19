0c2b1c72b416448e2e6a690dfc9d81c140fa8740
lucaspart7 <32098700+lucaspart7@users.noreply.github.com>
2017-09-19 15:02:15 +0200

    delete

--- .gitignore/index.php
+++ .gitignore/index.php
@@ -1,9 +1,1 @@
-<ul id="myUL">
-  <li><a href="https://www.youtube.com/channel/UCU0FhLr6fr7U9GOn6OiQHpQ">Officiel DEFAKATOR</a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-  <li><a href="#"></a></li>
-</ul>
+
